cb4b2dcec4252802180ba99f2b738492b3db4585
Paul Volle <p.volle@groupeonepoint.com>
2022-12-13 12:04:44 +0100

    Ex3

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    
+  end
+
+  def answer
+    @coach_answers = ["Silly question, get dressed and go to work!", "Great", "I don't care, get dressed and go to work!"]
+  end
+
+end
